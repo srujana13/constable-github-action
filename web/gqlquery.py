3042003c26ddf57a97180d984df8223bde72ff65
ayushjain94 <jain_ayush@outlook.com>
2020-10-27 03:23:14 -0400

    code formatted

--- web/gqlquery.py
+++ web/gqlquery.py
@@ -1,6 +1,6 @@
 class GQLQuery:
-    queries = {'GET_REPO_FILES' : 'get_repo_files',
-               'CHECK_VALID_REPO' : 'check_valid_repo',
+    queries = {'GET_REPO_FILES': 'get_repo_files',
+               'CHECK_VALID_REPO': 'check_valid_repo',
                'GET_CLOSED_ISSUES': 'get_closed_issues'}
 
     def __init__(self, path=None):
@@ -10,5 +10,7 @@ class GQLQuery:
             self.path = path
 
     def load(self, query_name):
-        if query_name not in self.queries: raise FileNotFoundError('Not a valid query name. Refer GQLQuery.queries')
+        if query_name not in self.queries:
+            raise FileNotFoundError(
+                'Not a valid query name. Refer GQLQuery.queries')
         return open(self.path + '{}.graphql'.format(self.queries[query_name])).read()
